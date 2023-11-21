dadfb8bd4c1e5e5828b542be9392621b67846667
peropero <a3046229569@163.com>
2023-11-21 18:41:07 +0800

    voj problem

---
+++ recursion/g.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+int a, b, c, d;
+bool count(int a, int b, int c, int d)
+{
+    if()   
+}
+void solve()
+{
+    count(a,b,c,d);
+    co
+}
+int main()
+{
+    while(scanf("%d %d %d %d", &a, &b, &c, &d) && a == 0)
+        solve();
+    return 0;
+}
--- recursion/i.cpp
+++ recursion/i.cpp
@@ -1,36 +1,61 @@
 //ans(num,status)
 #include <bits/stdc++.h>
 char a, b, c;
-int n;
+int n,cnt;
 typedef long long ll;
 void turn(int x, char a, char b)
 {
         printf("%d:%c->%c\n", x, a, b);
+        // printf("%d\n",++cnt);
 }
-void ans(int nh, bool flag)
+void ans(int nh, bool flag, bool Main, bool judge)
 {
-    if(nh == 1)
+    if(nh == 2)
     {
-        if(flag)
-            turn(1, b, a);
-        else 
+        if(flag && !Main)
+        {
+            turn(1,b,c);
+            turn(2,b,a);
+            turn(1,c,a);
+            
+        }
+        else if(!flag && !Main)
+        {
+            turn(1,a,c);
+            turn(2,a,b);
+            turn(1,c,b);
+        }
+        else if(flag && Main)
+        {
+            turn(1,b,a);
+            turn(2,b,c);
+            turn(1,a,c);
+        }
+        else
+        {
             turn(1,a,b);
+            turn(2,a,c);
+            turn(1,b,c);
+        }
     }
     else if(flag)
     {
-        ans(nh - 1, !flag);
+        ans(nh - 1, flag, false, false);
         turn(nh,b,c);
+        if(judge)ans(nh - 1, !flag, true, false);
+        else ans(nh - 1, !flag, false, false);
     }
     else
     {
-        ans(nh - 1, !flag);
+        ans(nh - 1, flag, false, false);
         turn(nh,a,c);
+        if(judge)ans(nh - 1, !flag, true, false);
+        else ans(nh - 1, !flag, false, false);
     }
-    a:;
 }
 int main()
 {
     std::cin >> n >> a >> b >> c;
-    ans(n, false);
+    ans(n, false,true,1);
     return 0;
 }
