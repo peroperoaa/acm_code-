2092b53b34259f36fec61ba42a061787cea86f7b
peropero <a3046229569@163.com>
2023-12-26 02:07:34 +0800

    sync

---
+++ luogu/lg.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+const int N = 3e6 + 10;
+signed main()
+{
+    vector<int> stk(N, 0), v(N, 0), ans(N, 0);
+    int top = 0;
+    int n;cin >> n;
+    for(int i = 0; i < n; i++)
+        cin >> v[i];
+    for(int i = n - 1; i >= 0; i--)
+    {
+        while(top && v[i] >= v[stk[top]])
+            top--;
+        if(top)
+            ans[i] =stk[top] + 1;
+        stk[++top] = i;
+    }
+    for(int i = 0; i < n; i++)
+        cout << ans[i] << ' ';
+    return 0;
+}
--- luogu/p2866.cpp
+++ luogu/p2866.cpp
@@ -1,30 +1,38 @@
 #include <bits/stdc++.h>
+#define long long int
 using namespace std;
 void solve(void)
 {
-    long long sum = 0;
-    vector<int> v, stk((int)8e4 + 10);
-    int n;
+    int sum = 0;
+    vector<int> v((int)8e4 + 10, 0), stk((int)8e4 + 10, 0);
+    int n, top = 0;
     cin >> n;
     for(int i = 0; i < n; i++)
+        cin >> v[i];
+    for(int i = n - 1; i >= 0; i--)
     {
-        int temp;cin >> temp;
-        v.push_back(temp);
-    }
-    reverse(v.begin(), v.end());
-    stk[0] = v[0];
-    int top = 0;
-    for(int i = 1; i < n; i++)
-    {
-        if(v[i] <= stk[top])
-            top = 0;
-        sum += top;
-        cout << sum << endl;
-        stk[++top] = v[i];
+        while(top && v[i] >= v[stk[top]])
+            top--;
+        if(top)
+            sum += stk[top] - i - 1;
+        else
+            sum += n - i - 1;
+        stk[++top] = i;
     }
     cout << sum;
+    // stk[0] = v[0];
+    // int top = 0;
+    // for(int i = 1; i < n; i++)
+    // {
+    //     if(v[i] <= stk[top])
+    //         top = 0;
+    //     sum += top;
+    //     cout << sum << endl;
+    //     stk[++top] = v[i];
+    // }
+    // cout << sum;
 }
-int main()
+signed main()
 {
     int _ = 1;
     while(_--)
