2c3635920f95f6983685b7a54eb7e84cd3f14a88
peropero <a3046229569@163.com>
2024-01-05 00:48:47 +0800

    sync

--- codeforce/918.cpp
+++ codeforce/918.cpp
@@ -1,26 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-char a[3][3];
+vector<int> v((int)2e5 + 10);
 void solve(void)
 {
-    int n;cin >> n;
-    long long sum = 0;
+    int n;
+    cin >> n;
     for(int i = 0; i < n; i++)
-    {
-        int temp;
-        cin >> temp;
-        sum += temp;
-    }
-    int sq = sqrt(sum);
-    if(sq * sq  == sum)
-        cout << "YES\n";
-    else    
-        cout << "NO\n";
+        if(i % 2 == 0)
+            cin >> v[i];
+        else
+        {
+            int temp;cin >> temp;
+            v[i] = -temp;
+        }
+    
 }
 int main()
 {
     ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
-    int t;
+    int t; 
     cin >> t;
     while(t--)
         solve();
