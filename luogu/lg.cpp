13bf3b836008eed07773e54524279b406d830ae5
peropero <a3046229569@163.com>
2024-01-04 14:15:40 +0800

    sync

--- luogu/lg.cpp
+++ luogu/lg.cpp
@@ -1,23 +1,24 @@
 #include <bits/stdc++.h>
 #define int long long
 using namespace std;
-const int N = 3e6 + 10;
+vector<int> v, stk(1000010);
+
 signed main()
 {
-    vector<int> stk(N, 0), v(N, 0), ans(N, 0);
-    int top = 0;
-    int n;cin >> n;
+    int n, top = 0;
+    cin >> n;
     for(int i = 0; i < n; i++)
-        cin >> v[i];
-    for(int i = n - 1; i >= 0; i--)
     {
-        while(top && v[i] >= v[stk[top]])
-            top--;
-        if(top)
-            ans[i] = stk[top] + 1;
-        stk[++top] = i;
+        int temp;
+        cin >> temp;
+        v.push_back(temp);
     }
-    for(int i = 0; i < n; i++)
-        cout << ans[i] << ' ';
+    for(int i = n - 1; i >=  0; i--)
+    {
+        while(top && v[i] > stk[top])
+            top--;
+        stk[++top] = v[i];
+    }
+    cout << stk[1] - stk[top];
     return 0;
 }
