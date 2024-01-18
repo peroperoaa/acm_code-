527fc9ef83f2c40e196f937afd8f614caebe680b
peropero <a3046229569@163.com>
2024-01-18 17:30:07 +0800

    synca

--- luogu/lg.cpp
+++ luogu/lg.cpp
@@ -1,24 +1,20 @@
 #include <bits/stdc++.h>
 #define int long long
 using namespace std;
-vector<int> v, stk(1000010);
-
 signed main()
 {
     int n, top = 0;
     cin >> n;
-    for(int i = 0; i < n; i++)
+    int p;
+    int ans = long LONG_LONG_MIN;
+    cin >> p;
+    for(int i = 1; i < n; i++)
     {
         int temp;
         cin >> temp;
-        v.push_back(temp);
+        if(ans < temp - p)ans = temp - p;
+        if(temp < p)p = temp;
     }
-    for(int i = n - 1; i >=  0; i--)
-    {
-        while(top && v[i] > stk[top])
-            top--;
-        stk[++top] = v[i];
-    }
-    cout << stk[1] - stk[top];
+    cout << ans;
     return 0;
 }
