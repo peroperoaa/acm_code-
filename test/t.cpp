2ba0f37a3bfb05255d1ea2836401fe1c6f94020a
peropero <a3046229569@163.com>
2024-03-03 14:15:04 +0800

    sync

--- test/t.cpp
+++ test/t.cpp
@@ -1,49 +1,14 @@
 #include <bits/stdc++.h>
-#define int long long 
 using namespace std;
-const int N = 1e5 + 10;
-int a[N];
-int dp[N];
-bool obb = 1, even;
-long long sumobb = -1111, sumeevn = -1111;
-void solve(void)
+ 
+struct stct{
+    int sb = true;
+};
+ 
+int main()
 {
-    //init
-    int n; cin >> n;
-    for(int i = 0; i < n; i++) cin >> a[i];
-    for(int i = 0; i < n; i++)
-    {
-        if(a[i] % 2)//obb
-        {
-            if(a[i] > 0 && obb)
-            {
-                if(sumobb > 0)sumobb+=a[i];
-                else sumobb=a[i];
-                    
-            }
-            else sumobb = max(sumobb, a[i]);
-            obb = 0;
-        }
-        else if(a[i] % 2)
-        {
-            if(a[i] > 0 && even)
-            {
-                if(sumobb > 0)sumobb+=a[i];
-                else sumobb=a[i];
-                    
-            }
-            else sumobb = max(sumobb, a[i]);
-        }
-        else
-        {
-
-        }
-    }
-}
-signed main()
-{
-    ios::sync_with_stdio(0), cin.tie(0);
-    int _; cin >> _;
-    while(_--) solve();
+    stct *lxh = new stct ({true});
+    if(lxh -> sb) cout << "True\n";
+    else cout << "False\n";
     return 0;
 }
--- test/test.cpp
+++ test/test.cpp
@@ -1,9 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
+void solve(void)
+{
+    double n; cin >> n;
+    if(n >= 85) cout << 'A' << endl;
+    else if(n >= 75) cout << 'B' << endl;
+    else if(n >= 65) cout << 'C' << endl;
+    else if(n >= 60) cout << 'D' << endl;
+    else cout << 'F' << endl;
+}
 int main()
 {
-    int a = 10;
-    int * p = &a;
-    cout << (int)&a << endl;
+    int t = 1;
+    cin >> t;
+    while(t--)
+        solve();
     return 0;
+}
+int * rtr(void)
+{
+    int * p = new int [10];
+
+    //
+
+    
+    return p;
 }
