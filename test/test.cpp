4aa16b069276e60d23346390f558510a52dfda0a
peropero <a3046229569@163.com>
2024-03-04 12:45:48 +0800

    sync

--- test/test.cpp
+++ test/test.cpp
@@ -1,28 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
-void solve(void)
-{
-    double n; cin >> n;
-    if(n >= 85) cout << 'A' << endl;
-    else if(n >= 75) cout << 'B' << endl;
-    else if(n >= 65) cout << 'C' << endl;
-    else if(n >= 60) cout << 'D' << endl;
-    else cout << 'F' << endl;
-}
 int main()
 {
-    int t = 1;
-    cin >> t;
-    while(t--)
-        solve();
+    int a = 10;
+    int 
     return 0;
-}
-int * rtr(void)
-{
-    int * p = new int [10];
-
-    //
-
-    
-    return p;
 }
