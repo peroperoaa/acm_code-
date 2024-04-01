8c129aa885e5190dd9b7e712f42804de08f9d9e4
peropero <a3046229569@163.com>
2024-04-01 22:05:39 +0800

    sync

--- test/t.cpp
+++ test/t.cpp
@@ -1,52 +1,30 @@
 #include <bits/stdc++.h>
+#define int long long 
 using namespace std;
-const int N = 5e2 + 10;
-struct node {
-    int id;
-    int r;
-};
-bool cmp(node a, node b)
+
+void solve(void)
 {
-    if(a.r != b.r)
-        return a.r < b.r;
-    else a.id < b.id;
-}
-vector<node> ans;
-vector<int> g[N];
-int n, m, in[N];
-void toposort(void)
-{
-    queue<node> q;
-    for(int i = 1; i <= n; i++)
-        if(in[i] == 0)
-        {
-            q.push({i, 0}), ans.push_back({i, 0});
-            break;
-        }
-    while(q.size())
+    int n; cin >> n;
+    vector<int> v;
+    for(int i = 0; i < n; i++)
     {
-        node temp = q.front();
-        q.pop();
-        for(int i : g[temp.id])
-            if(--in[i] == 0)
-            {
-                q.push({i, temp.r + 1}), ans.push_back({i, temp.r + 1});
-                break;
-            }
+        int temp; cin >> temp;
+        v.push_back(temp);
     }
+    sort(v.begin(), v.end());
+    int  mn = LONG_LONG_MAX;
+    for(int i = 0; i < n - 1; i++)
+        mn = min(mn, v[i + 1] - v[i]);
+    // cout << mn;
+    if(mn == 0)
+    cout << n << endl;
+    else
+    cout << (v[n - 1] - v[0]) / mn + 1;
 }
-int main()
+signed main()
 {
-    cin >> n >> m;
-    for(int i = 0; i < m; i++)
-    {
-        int f, l; cin >> f >> l;
-        g[f].push_back(l);
-        in[l]++;
-    }
-    toposort();
-    sort(ans.begin(), ans.end(), cmp);
-    for(node a : ans)
-        cout << a.id << ' ';
+    int t = 1; 
+    // cin >> t;
+    while(t--) solve();   
     return 0;
 }
